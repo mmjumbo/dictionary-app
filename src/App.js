89d92fd1e667f72c6f9102967efee7f613a8646b
WORKBLYS <dev@workblys.com>
2025-02-18 11:21:43 -0500

    Set up a dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.avif';
-import './App.css';
+import logo from "./logo.avif";
+import Dictionary from "./Dictionary";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>This SheCodes project is created by Maggie</small>
+          </footer>
+      </div>
     </div>
   );
 }
-

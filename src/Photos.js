aae35571a2a42ca6446e68444c427e40d0f49ec9
WORKBLYS <dev@workblys.com>
2025-03-08 14:58:23 -0500

    added alt word to photos

--- src/Photos.js
+++ src/Photos.js
@@ -1,20 +1,25 @@
 import React from "react";
-import "./Photos.css"
+import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
         <div className="row">
-        {props.photos.map(function (photo, index) {
-          return (
-          <div className="col-4 key={index}">  
-          <a href={photo.src.original} target="_blank" rel="noreferrer">
-          <img src={photo.src.landscape} className="img-fluid"/>
-            </a>
-          </div>
-         )
-        })}
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4 key={index}">
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  return{" "}
+                  <img
+                    src={photo.src.landscape}
+                    alt={photo.alt || "Photo of " + props.keyword}
+                  />
+                  ;
+                </a>
+              </div>
+            );
+          })}
         </div>
       </section>
     );
